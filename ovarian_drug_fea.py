dfdc2a42529485cc61770968497cded2c8b57dd8
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-07-16 08:59:16 -0700

    add assertion

--- ovarian_drug_fea.py
+++ ovarian_drug_fea.py
@@ -1,3 +1,6 @@
+"""
+Create separate drug fea data files for ovarian drugs.
+"""
 import os
 from pathlib import Path
 import pandas as pd
@@ -17,20 +20,17 @@ fps_prefix = 'ecfp4.'
 # Load Ovarian drugs
 ov_drugs = pd.read_csv(datadir / 'Drugs_For_OV_Proposal_Analysis.txt', sep='\t')
 ov_drugs = ov_drugs.sort_values(by='drug_name')
+ov_drugs = ov_drugs[ov_drugs[id_name] != 'Drug_18'] # remove Cisplatin dup Drug_18 (keep Drug_42)
 
-# Remove Cisplatin duplicate (remove Drug_18, keep Drug_42)
-ov_drugs = ov_drugs[ov_drugs[id_name] != 'Drug_18']
-
-# Load csa benchmark feature data
-# info = pd.read_csv(datadir / 'drug_info.tsv', sep='\t')
+# Load csa benchmark drug feature data
 info = pd.read_csv(datadir / 'csa_data/raw_data/x_data' / 'drug_info.tsv', sep='\t')
-mrd = pd.read_csv(datadir / 'csa_data/raw_data/x_data' / 'drug_mordred.tsv', sep='\t')
-fps = pd.read_csv(datadir / 'csa_data/raw_data/x_data' / 'drug_ecfp4_nbits512.tsv', sep='\t')
+mrd  = pd.read_csv(datadir / 'csa_data/raw_data/x_data' / 'drug_mordred.tsv', sep='\t')
+fps  = pd.read_csv(datadir / 'csa_data/raw_data/x_data' / 'drug_ecfp4_nbits512.tsv', sep='\t')
 
 # Merge ovarian drugs with csa benchmark feature data
-mrd_merged = pd.merge(ov_drugs, mrd, on='improve_chem_id')
-fps_merged = pd.merge(ov_drugs, fps, on='improve_chem_id')
 info_merged = pd.merge(ov_drugs, info, on='improve_chem_id')
+mrd_merged  = pd.merge(ov_drugs, mrd, on='improve_chem_id')
+fps_merged  = pd.merge(ov_drugs, fps, on='improve_chem_id')
 
 # Relevant columns
 mrd_cols = [c for c in mrd_merged.columns if mordred_prefix in c]
@@ -38,21 +38,22 @@ fps_cols = [c for c in fps_merged.columns if fps_prefix in c]
 mrd_merged = mrd_merged[[id_name] + mrd_cols]
 fps_merged = fps_merged[[id_name] + fps_cols]
 
+# print(info_merged.shape)
 # print(mrd_merged.shape)
 # print(fps_merged.shape)
-# print(info_merged.shape)
 
+# print(info_merged.iloc[:10])
 # print(mrd_merged.iloc[:10, :10])
 # print(fps_merged.iloc[:10, :10])
-# print(info_merged.iloc[:10])
 
 # Remove SMILES duplicates
 smi = info_merged.drop_duplicates(subset=['drug_name', id_name, 'canSMILES'])
 print(smi.shape)
-print(smi.iloc[:, :8])
+# print(smi.iloc[:, :8])
 
 # Remove NSC duplicates (keep CTRP, GDSC, etc. instead)
 dups = smi[smi.duplicated(subset=['drug_name', id_name], keep=False)]
+# print(dups.iloc[:, :4])
 dups_nsc = dups[[True if 'NSC' in i else False for i in dups['DrugID']]]
 dups_nsc_list = dups_nsc['DrugID'].values
 # print(smi.shape)
@@ -62,14 +63,18 @@ smi = smi[~smi['DrugID'].isin(dups_nsc_list)]
 smi = smi[[id_name, 'canSMILES']]
 
 # Save data
-mrd_merged.to_csv(outdir / 'drug_mordred_ovarian.tsv', sep='\t', index=False)
-fps_merged.to_csv(outdir / 'drug_ecfp4_nbits512_ovarian.tsv', sep='\t', index=False)
-info_merged.to_csv(outdir / 'drug_info_ovarian.tsv', sep='\t', index=False)
-smi.to_csv(outdir / 'drug_SMILES_ovarian.tsv', sep='\t', index=False)
+info_merged.to_csv(outdir / 'drug_info_ovarian.tsv', sep='\t', index=False)  # info
+mrd_merged.to_csv(outdir / 'drug_mordred_ovarian.tsv', sep='\t', index=False)  # mordred
+fps_merged.to_csv(outdir / 'drug_ecfp4_nbits512_ovarian.tsv', sep='\t', index=False)  # ecfp4
+smi.to_csv(outdir / 'drug_SMILES_ovarian.tsv', sep='\t', index=False) # SMILES
 
-print("\nMordred:\n", mrd_merged.iloc[:4, :10])
-print("\nECFP4:\n", fps_merged.iloc[:4, :10])
-print("\nInfo:\n", info_merged.iloc[:4])
+print("\nMordred:\n", mrd_merged.iloc[:3, :7])
+print("\nECFP4:\n", fps_merged.iloc[:3, :7])
+print("\nInfo:\n", info_merged.iloc[:3])
 print("\nSMILES:\n", smi) # .iloc[:4])
+
+# Check if all all drugs from ov_drugs are already in drugs_mordred
+aa = mrd[mrd[id_name].isin(ov_drugs[id_name])]
+assert aa.shape[0] == mrd_merged.shape[0], "missing samples"
 
 print("Finished.")
