8133297fcef3464e9ead8cf332c5bd1999bd1f7c
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-07-15 19:46:00 -0700

    install mordredcommunity instead of mordred

--- conda_env.sh
+++ conda_env.sh
@@ -4,6 +4,8 @@ set -e
 
 # conda create -n ovarian python=3.9 pip pandas joblib pyarrow rdkit mordred scikit-learn
 
+# conda create -n ovarian python=3.9 pip pandas joblib pyarrow rdkit mordredcommunity scikit-learn
+
 # My packages
 conda install -c conda-forge ipdb=0.13.11 --yes
 conda install -c conda-forge python-lsp-server=1.2.4 --yes
